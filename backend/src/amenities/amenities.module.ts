580b6662ddf030e65ea63e7a5f5f3e8a734269c6
FINNxFASTEST <ballzaee200991@gmail.com>
2026-04-25 23:40:09 +0700

    refactor amenities domain to clean architechture

--- backend/src/amenities/amenities.module.ts
+++ backend/src/amenities/amenities.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AmenitiesPersistenceModule } from './infrastructure/amenities-persistence.module';
-import { AmenitiesController } from './amenities.controller';
-import { AmenitiesService } from './amenities.service';
+import { AmenitiesController } from './presentation/amenity.controller';
+import { CreateAmenityUseCase } from './application/use-cases/create-amenity.use-case';
+import { FindAmenitiesUseCase } from './application/use-cases/find-amenities.use-case';
 
 @Module({
   imports: [AmenitiesPersistenceModule],
   controllers: [AmenitiesController],
-  providers: [AmenitiesService],
-  exports: [AmenitiesService],
+  providers: [CreateAmenityUseCase, FindAmenitiesUseCase],
+  exports: [FindAmenitiesUseCase],
 })
 export class AmenitiesModule {}
---
+++ backend/src/amenities/application/use-cases/create-amenity.use-case.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { AmenityRepository } from '../../infrastructure/persistence/amenity.repository';
+import { Amenity } from '../../domain/amenity';
+import { CreateAmenityDto } from '../../presentation/dto/create-amenity.dto';
+
+@Injectable()
+export class CreateAmenityUseCase {
+  constructor(private readonly amenityRepository: AmenityRepository) {}
+
+  execute(dto: CreateAmenityDto): Promise<Amenity> {
+    return this.amenityRepository.create({
+      label: dto.label,
+      iconKey: dto.iconKey,
+    });
+  }
+}
