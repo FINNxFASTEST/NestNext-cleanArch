9fee5719a30e5afa475e963c5297025e268d21bd
FINNxFASTEST <ballzaee200991@gmail.com>
2026-04-25 23:52:39 +0700

    remove warning

--- backend/src/roles/domain/role.ts
+++ backend/src/roles/domain/role.ts
@@ -1,12 +1,9 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { Allow } from 'class-validator';
 
 export class Role {
   @Allow()
-  @ApiProperty({ type: String })
-  id: number | string;
+  id!: number | string;
 
   @Allow()
-  @ApiProperty({ type: String, example: 'admin' })
   name?: string;
 }
--- backend/src/users/domain/user.ts
+++ backend/src/users/domain/user.ts
@@ -1,53 +1,24 @@
 import { Exclude, Expose } from 'class-transformer';
 import { Role } from '../../roles/domain/role';
 import { Status } from '../../statuses/domain/status';
-import { ApiProperty } from '@nestjs/swagger';
 
 export class User {
-  @ApiProperty({ type: String })
-  id: string;
+  id!: string;
 
-  @ApiProperty({
-    type: String,
-    example: 'john.doe@example.com',
-  })
   @Expose({ groups: ['me', 'admin'] })
-  email: string | null;
+  email!: string | null;
 
   @Exclude({ toPlainOnly: true })
   password?: string;
 
-  @ApiProperty({
-    type: String,
-    example: 'email',
-  })
   @Expose({ groups: ['me', 'admin'] })
-  provider: string;
+  provider!: string;
 
-  @ApiProperty({
-    type: String,
-    example: 'John',
-  })
-  firstName: string | null;
-
-  @ApiProperty({
-    type: String,
-    example: 'Doe',
-  })
-  lastName: string | null;
-
-  @ApiProperty({ type: () => Role })
+  firstName!: string | null;
+  lastName!: string | null;
   role?: Role | null;
-
-  @ApiProperty({ type: () => Status })
   status?: Status;
-
-  @ApiProperty()
-  createdAt: Date;
-
-  @ApiProperty()
-  updatedAt: Date;
-
-  @ApiProperty()
-  deletedAt: Date;
+  createdAt!: Date;
+  updatedAt!: Date;
+  deletedAt!: Date;
 }
